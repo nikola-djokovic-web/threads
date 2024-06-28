c4a702ae9cf149f503a9f939c07105ef17e25992
nikola-djokovic-web <djokovicnikola69@gmail.com>
2024-06-28 13:30:23 +0200

    first commit - onboarding

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  );
+}
